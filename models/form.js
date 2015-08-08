c1194e319c23736250f3b1662083691a6bdad32f
TanczaceKotki <k.bigaj@uj.edu.pl>
2015-08-08 23:16:00 +0200

    ok

--- models/form.js
+++ models/form.js
@@ -1,3 +1,27 @@
-module.exports = function fractionsXML() {
+var Fraction = function () {};
 
+Fraction.prototype.getXML = function () {
+    try {
+        var rawJSON;
+        var xml2js = require('xml2js');
+        var fs = require('fs');
+        var fileData = fs.readFileSync('./public/xml/xml.xml', 'ascii');
+        var parser = new xml2js.Parser({explicitArray : false});
+        parser.parseString(fileData.substring(0, fileData.length), function (err, result) {
+            rawJSON = result;
+        });
+        return rawJSON.root.fraction;
+    } catch (ex) {console.log(ex)}
 };
+
+Fraction.prototype.rawJSON = Fraction.prototype.getXML();
+
+Fraction.prototype.list = function () {
+    var f = [];
+    Fraction.prototype.rawJSON.forEach(function(entry){
+        f.push(entry.title);
+    });
+    return f;
+};
+
+module.exports = new Fraction();
--- routes/index.js
+++ routes/index.js
@@ -1,17 +1,16 @@
 var express = require('express');
 var router = express.Router();
 var fs = require('fs');
-var xml2js = require('xml2js');
-
-var xmlPath = './public/xml/xml.xml';
-var fractionList = [];
+var f = require('../models/form');
+var rawJSON = f.getXML();
+var fractionList = f.list();
 
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  var rawJSON = loadXMLDoc( xmlPath );
+
   res.render('index', {
-    frac: fractions(rawJSON)
+    frac: rawJSON
   })
 });
 
@@ -21,14 +20,11 @@ router.get('/add-fraction', function(req, res){
 
 // GET fraction units stats
 router.get('/:fraction', function(req, res) {
-  var rawJSON = loadXMLDoc( xmlPath );
-  var fract = fracList(rawJSON);
-  var cmp = fract.indexOf(req.params.fraction);
+  var cmp = fractionList.indexOf(req.params.fraction);
   if (cmp > -1) {
-    var details = returnFractionDetails(rawJSON, cmp);
-    res.render('frac', {frac: fractions(rawJSON), stats: details});
+    res.render('frac', {frac: rawJSON, stats: rawJSON[cmp]});
   } else {
-    res.render('frac-404', {frac: fractions(rawJSON)});
+    res.render('frac-404', {frac: rawJSON});
   }
 });
 
@@ -61,35 +57,3 @@ router.post('/add', function(req, res) {
 
 module.exports = router;
 
-
-function loadXMLDoc(filePath) {
-  try {
-    var fileData = fs.readFileSync(filePath, 'ascii');
-    var parser = new xml2js.Parser({explicitArray : false});
-    parser.parseString(fileData.substring(0, fileData.length), function (err, result) {
-      json = result;
-    });
-    return json;
-  } catch (ex) {console.log(ex)}
-}
-
-function fractions(rawJSON) {
-  var fractions = [];
-  rawJSON.root.fraction.forEach(function(entry){
-    fractions.push(entry);
-  });
-  return fractions;
-}
-
-function fracList(rawJSON) {
-  if (fractionList.length == 0) {
-    rawJSON.root.fraction.forEach(function(entry){
-      fractionList.push(entry.title);
-    });
-  }
-  return fractionList;
-}
-
-function returnFractionDetails(rawJSON, fracIndex) {
-  return rawJSON.root.fraction[fracIndex];
-}
